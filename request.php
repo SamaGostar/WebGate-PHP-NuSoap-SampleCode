503882607a67a6a7dd99f303b57162c661d4dc9d
Masoud A <amini141@gmail.com>
2013-08-28 16:27:00 +0430

    Fix UTF-8 Encoding

--- request.php
+++ request.php
@@ -12,7 +12,7 @@
 	
 	// URL also Can be https://ir.zarinpal.com/pg/services/WebGate/wsdl
 	$client = new nusoap_client('https://de.zarinpal.com/pg/services/WebGate/wsdl', array('encoding' => 'UTF-8')); 
-	
+	$client->soap_defencoding = 'UTF-8';
 	$result = $client->call("PaymentRequest", array(
 													array(
 															'MerchantID' 	=> $MerchantID,
